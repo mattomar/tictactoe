d21bbaae5b8213209037e9d38d71e36933989e2f
mattomar <moddathero@gmail.com>
2023-08-25 16:39:32 +0700

    added the functions for checking the winner and editted the layout

--- javascript.js
+++ javascript.js
@@ -1,37 +1,107 @@
-const gameBoard = [
-    { id: 1, value: '' },
-    { id: 2, value: '' },
-    { id: 3, value: '' },
-    { id: 4, value: '' },
-    { id: 5, value: '' },
-    { id: 6, value: '' },
-    { id: 7, value: '' },
-    { id: 8, value: '' },
-    { id: 9, value: '' }
-];
+(function () {
+    function startTicTacToeGame() {
+        const gameBoard = [
+            { id: 1, value: '' },
+            { id: 2, value: '' },
+            { id: 3, value: '' },
+            { id: 4, value: '' },
+            { id: 5, value: '' },
+            { id: 6, value: '' },
+            { id: 7, value: '' },
+            { id: 8, value: '' },
+            { id: 9, value: '' }
+        ];
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const startButton = document.getElementById('start-button');
+            const choiceForm = document.getElementById('choice-form');
+            const form = choiceForm.querySelector('form');
+            const gameBoardElement = document.getElementById('game-board');
+            const cells = gameBoardElement.querySelectorAll('.cell');
+
+            choiceForm.style.display = 'none';
+            gameBoardElement.style.display = 'none';
+
+            let currentPlayer = 'X';
+            let selectedCells = [];
+
+            startButton.addEventListener('click', function () {
+                startButton.style.display = 'none';
+                choiceForm.style.display = 'block';
+            });
+
+            form.addEventListener('submit', function (e) {
+                e.preventDefault();
+                const formData = new FormData(form);
+                const player1Choice = formData.get('choice');
+                const player2Choice = player1Choice === 'X' ? 'O' : 'X';
+                alert(`Player 1 chose "${player1Choice}"\nPlayer 2 gets "${player2Choice}"`);
+                choiceForm.style.display = 'none';
+                gameBoardElement.style.display = 'grid';
+
+                cells.forEach(cell => {
+                    cell.addEventListener('click', function () {
+                        if (!selectedCells.includes(cell.id) && cell.textContent === '') {
+                            cell.textContent = currentPlayer;
+                            selectedCells.push(cell.id);
+                            gameBoard[cell.id - 1].value = currentPlayer;
+
+                            if (checkWin(currentPlayer)) {
+                                alert(`Player ${currentPlayer} wins!`);
+                                resetGame();
+                            } else if (checkDraw()) {
+                                alert("It's a draw!");
+                                resetGame();
+                            } else {
+                                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                            }
+                        }
+                    });
+                });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const startButton = document.getElementById('start-button');
-    const choiceForm = document.getElementById('choice-form');
-    const form = choiceForm.querySelector('form');
+                function checkWin(player) {
+                    const winningCombos = [
+                        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+                        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+                        [0, 4, 8], [2, 4, 6]             
+                    ];
 
-    // Initially hide the choice form
-    choiceForm.style.display = 'none';
+                    
+                    for (const combo of winningCombos) {
+                        const [a, b, c] = combo;
+                        if (
+                            gameBoard[a].value === player &&
+                            gameBoard[b].value === player &&
+                            gameBoard[c].value === player
+                        ) {
+                            return true; 
+                        }
+                    }
+                    return false; 
+                }
 
-    // Listen for the "Start Game" button click
-    startButton.addEventListener('click', function () {
-        startButton.style.display = 'none'; // Hide the start button
-        choiceForm.style.display = 'block'; // Show the choice form
-    });
+                function checkDraw() {
+                    return selectedCells.length === 9; 
+                }
 
-    // Listen for the form submission
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const formData = new FormData(form);
-        const player1Choice = formData.get('choice');
-        const player2Choice = player1Choice === 'X' ? 'O' : 'X';
-        alert(`Player 1 chose "${player1Choice}"\nPlayer 2 gets "${player2Choice}"`);
-        choiceForm.style.display = 'none'; // Hide the choice form after submission
-    });
-});
+                
+                function resetGame() {
+                    
+                    cells.forEach(cell => {
+                        cell.textContent = '';
+                    });
+
+                
+                    currentPlayer = 'X';
+                    selectedCells = [];
+                    gameBoard.forEach(cell => {
+                        cell.value = '';
+                    });
+                }
+            });
+        });
+    }
+
+    startTicTacToeGame();
+})();
